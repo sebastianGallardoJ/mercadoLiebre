5c633454e002b5f081376490968c5cb2f9711181
SebastianGallardoJ <gallardosebastian868@gmail.com>
2022-12-12 00:13:16 -0300

    agrega toda la aplicacion al github

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname,'public')))
+
+app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'views','index.html')))
+app.get('/ofertas',(req,res)=> res.sendFile(path.resolve(__dirname,'views','ofertas.html')))
+
+
+
+app.listen(3030,()=> "servidor levantado en puerto 3030")
