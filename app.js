55cfacc3e6e45bb9f6ed326951a448d2147a4693
SebastianGallardoJ <gallardosebastian868@gmail.com>
2023-01-30 18:55:12 -0300

    trabajo de ML terminado

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(express.static(path.resolve(__dirname,'public')))
 
 app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'views','index.html')))
-app.get('/ofertas',(req,res)=> res.sendFile(path.resolve(__dirname,'views','ofertas.html')))
-
+app.get('/register',(req,res)=> res.sendFile(path.resolve(__dirname,'views','register.html')))
+app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'views','login.html')))
 
 
 app.listen(3030,( ) => "servidor levantado en puerto 3030")
